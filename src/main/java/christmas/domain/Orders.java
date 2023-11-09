aa7366832f0e03005fd72cdc29dcc29a6fd50594
leedohyun <lucky77795@naver.com>
2023-11-10 06:10:05 +0900

    feat(christmas) : 메뉴와 메뉴 주문 개수를 받을 수 있는 Orders 클래스 작성.

---
+++ src/main/java/christmas/domain/Orders.java
@@ -0,0 +1,43 @@
+package christmas.domain;
+
+import christmas.domain.food.Food;
+import christmas.util.Parser;
+import christmas.util.validator.MenuCountValidator;
+import christmas.util.validator.MenuValidator;
+import christmas.util.validator.OrderValidator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Orders {
+
+    private final Map<Food, Integer> orders;
+
+    private Orders(String orders) {
+        validate(orders);
+        this.orders = registerOrders(orders);
+    }
+
+    public static Orders create(String orders) {
+        return new Orders(orders);
+    }
+
+    private void validate(String orders) {
+        OrderValidator.validateOrders(orders);
+    }
+
+    private Map<Food, Integer> registerOrders(String orders) {
+        List<String> menus = Parser.parseMenus(orders);
+        List<Integer> counts = Parser.parseMenuCounts(orders);
+        Map<Food, Integer> order = new HashMap<>();
+
+        for (int idx = 0; idx < menus.size(); idx++) {
+            String menuName = menus.get(idx);
+            Food food = Menu.getFoodByName(menuName);
+            int count = counts.get(idx);
+
+            order.put(food, count);
+        }
+        return order;
+    }
+}
