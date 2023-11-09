b2386c0efd12b78a7653699006163d698bbae3e6
leedohyun <lucky77795@naver.com>
2023-11-09 21:13:49 +0900

    test(christmas) : VisitDate Test 작성.

---
+++ src/test/java/christmas/domain/VisitDateTest.java
@@ -0,0 +1,38 @@
+package christmas.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayName("방문 날짜 입력 값 테스트")
+class VisitDateTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "15", "31"})
+    @DisplayName("정상 값 입력 테스트")
+    void validValueInputTest(String dateValidInputValues) {
+        Assertions.assertDoesNotThrow(() -> VisitDate.create(dateValidInputValues));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1일", "one", "1 3", "-1"})
+    @DisplayName("숫자가 아닌 값을 입력했을 때 예외 발생")
+    void invalidValueInputTest(String dateInvalidFormatInputValues) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+           VisitDate.create(dateInvalidFormatInputValues);
+        });
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "32"})
+    @DisplayName("1~31 까지의 숫자가 아닌 값을 입력했을 때 예외 발생")
+    void invalidRangeInputTest(String dateInvalidRangeInputValues) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            VisitDate.create(dateInvalidRangeInputValues);
+        });
+    }
+}
