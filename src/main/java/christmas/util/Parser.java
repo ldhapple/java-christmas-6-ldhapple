b7e31e28dda6ef0dd0709c9e261a911d32fe8063
leedohyun <lucky77795@naver.com>
2023-11-10 06:51:54 +0900

    fix(christmas) : 정규 표현식 잘못 사용한 부분 수정.
    두 자리 이상의 숫자가 나오면 메뉴를 잘못 추출하던 부분 수정.

--- src/main/java/christmas/util/Parser.java
+++ src/main/java/christmas/util/Parser.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Parser {
-    private static final String extractMenusRegex = "-\\d,*";
+    private static final String extractMenusRegex = "-\\d+,*";
     private static final String extractCountsRegex = ",*[가-힣]+-";
 
     public static int parseDate(String date) {
