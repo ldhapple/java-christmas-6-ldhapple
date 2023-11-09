14215880bdc8d9ba3586b9973a6ac584970660d2
leedohyun <lucky77795@naver.com>
2023-11-09 19:35:55 +0900

    feat(christmas) : 예상 방문 일자 구현 및 검증 구현

---
+++ src/main/java/christmas/domain/VisitDate.java
@@ -0,0 +1,48 @@
+package christmas.domain;
+
+import java.util.regex.Pattern;
+
+public class VisitDate {
+    private static final Pattern visitDateRegex = Pattern.compile("\\d+");
+    //숫자 1개 이상일 경우 허용
+
+    private final int date;
+
+    private VisitDate(String date) {
+        validate(date);
+        this.date = parseDate(date);
+    }
+
+    public static VisitDate create(String date) {
+        return new VisitDate(date);
+    }
+
+    private void validate(String date) {
+        validateFormat(date);
+        validateValidDate(parseDate(date));
+    }
+
+    private void validateFormat(String date) {
+        if (isInvalidFormat(date)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateValidDate(int date) {
+        if (isInValidRange(date)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isInvalidFormat(String date) {
+        return !visitDateRegex.matcher(date).matches();
+    }
+
+    private boolean isInValidRange(int date) {
+        return date < 1 || date > 31;
+    }
+
+    private static int parseDate(String date) {
+        return Integer.parseInt(date);
+    }
+}
