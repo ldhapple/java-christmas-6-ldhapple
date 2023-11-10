626fe3d239a162d7316e1f08d021b0e5ec014e93
leedohyun <lucky77795@naver.com>
2023-11-11 06:38:40 +0900

    feat(christmas) : 컨트롤러에 필요한 값들을 계산해주는 EventCalculator 구현.

---
+++ src/main/java/christmas/service/EventCalculator.java
@@ -0,0 +1,88 @@
+package christmas.service;
+
+import christmas.domain.Orders;
+import christmas.domain.VisitDate;
+import christmas.domain.event.DiscountPolicy;
+import christmas.domain.food.Food;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class EventCalculator {
+
+    public int getOrderTotalAmount(Orders orders) {
+        int orderTotalPrice = 0;
+
+        Map<Food, Integer> order = orders.getOrders();
+
+        for (Entry<Food, Integer> entry : order.entrySet())
+        {
+            Food orderFood = entry.getKey();
+            int orderFoodPrice = orderFood.getPrice();
+            int orderFoodCount = entry.getValue();
+
+            orderTotalPrice += orderFoodPrice * orderFoodCount;
+        }
+
+        return orderTotalPrice;
+    }
+
+    public EnumMap<DiscountPolicy, Integer> getDiscountResult (List<DiscountPolicy> discountPolicies, VisitDate visitDate, Orders orders) {
+        EnumMap<DiscountPolicy, Integer> results = new EnumMap<>(DiscountPolicy.class);
+
+        for (DiscountPolicy discountPolicy : discountPolicies) {
+            int requiredValue = getRequiredValue(visitDate, orders, discountPolicy);
+            int discountAmount = discountPolicy.getDiscountAmount(requiredValue);
+
+            results.put(discountPolicy, discountAmount);
+        }
+
+        return results;
+    }
+
+    public int getTotalBenefitAmount(EnumMap<DiscountPolicy, Integer> discountResults, Food benefitFood) {
+        int totalDiscountAmount = 0;
+        for (int discountAmount : discountResults.values()) {
+            totalDiscountAmount += discountAmount;
+        }
+
+        if (benefitFood != null) {
+            totalDiscountAmount += benefitFood.getPrice() * -1;
+        }
+
+        return totalDiscountAmount;
+    }
+
+    public int getExpectedPayAmount(int orderTotalAmount, int benefitAmount) {
+        return orderTotalAmount + benefitAmount;
+    }
+
+    private int getRequiredValue(VisitDate visitDate, Orders orders, DiscountPolicy discountPolicy) {
+        if (isChristmasDiscount(discountPolicy)) {
+            return visitDate.getDate();
+        }
+
+        if (isWeekDayDiscount(discountPolicy)) {
+            return orders.getDessertMenuCount();
+        }
+
+        if (isWeekEndDiscount(discountPolicy)) {
+            return orders.getMainMenuCount();
+        }
+
+        return 0;
+    }
+
+    private boolean isWeekEndDiscount(DiscountPolicy discountPolicy) {
+        return discountPolicy.equals(DiscountPolicy.WEEKEND_DISCOUNT);
+    }
+
+    private boolean isWeekDayDiscount(DiscountPolicy discountPolicy) {
+        return discountPolicy.equals(DiscountPolicy.WEEK_DAY_DISCOUNT);
+    }
+
+    private boolean isChristmasDiscount(DiscountPolicy discountPolicy) {
+        return discountPolicy.equals(DiscountPolicy.CHRISTMAS_DISCOUNT);
+    }
+}
