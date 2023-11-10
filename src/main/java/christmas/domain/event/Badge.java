eff50f4caa117e1c22c913ded4926c700dd670ed
leedohyun <lucky77795@naver.com>
2023-11-11 06:33:07 +0900

    feat(christmas) : 정의된 조건에 알맞은 배지를 찾는 기능 구현

--- src/main/java/christmas/domain/event/Badge.java
+++ src/main/java/christmas/domain/event/Badge.java
@@ -1,13 +1,14 @@
 package christmas.domain.event;
 
+import java.util.Arrays;
 import java.util.function.Predicate;
 
 public enum Badge {
 
-    NO_BADGE("없음", (benefitAmount) -> benefitAmount < 5_000),
-    STAR_BADGE("별", (benefitAmount) -> benefitAmount >= 5_000),
-    TREE_BADGE("트리", (benefitAmount) -> benefitAmount >= 10_000),
-    SANTA_BADGE("산타", (benefitAmount) -> benefitAmount >= 20_000);
+    NO_BADGE("없음", (benefitAmount) -> benefitAmount * -1 < 5_000),
+    SANTA_BADGE("산타", (benefitAmount) -> benefitAmount * -1 >= 20_000),
+    TREE_BADGE("트리", (benefitAmount) -> benefitAmount * -1 >= 10_000),
+    STAR_BADGE("별", (benefitAmount) -> benefitAmount * -1 >= 5_000);
 
     private final String name;
     private final Predicate<Integer> checker;
@@ -15,5 +16,16 @@ public enum Badge {
     Badge(String name, Predicate<Integer> checker) {
         this.name = name;
         this.checker = checker;
+    }
+
+    public static Badge findBadge(int benefitAmount) {
+        return Arrays.stream(Badge.values())
+                .filter((badge) -> badge.checker.test(benefitAmount))
+                .findFirst()
+                .orElse(NO_BADGE);
+    }
+
+    public String getName() {
+        return name;
     }
 }
