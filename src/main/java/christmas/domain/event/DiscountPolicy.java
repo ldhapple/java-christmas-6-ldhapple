cdf48b3e98f54757b3ca8e11f62a818350520cb4
leedohyun <lucky77795@naver.com>
2023-11-10 19:04:00 +0900

    fix(christmas) : 테스트 중 발견한 오류 수정.
    수식 잘못되었던 부분 수정.

--- src/main/java/christmas/domain/event/DiscountPolicy.java
+++ src/main/java/christmas/domain/event/DiscountPolicy.java
@@ -1,4 +1,4 @@
-package christmas.domain;
+package christmas.domain.event;
 
 import static christmas.domain.DecemberCalendar.*;
 
@@ -6,10 +6,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum DiscountPolicy {
+
     CHRISTMAS_DISCOUNT((date) -> 1_000 + ((date - 1) * 100),
             (date) -> date >= CHRISTMAS_EVENT_PERIOD.getFirstDay() && date <= CHRISTMAS_EVENT_PERIOD.getSecondDay()),
     WEEKEND_DISCOUNT((mainMenuCount) -> 2_023 * mainMenuCount,
@@ -17,7 +17,7 @@ public enum DiscountPolicy {
     STAR_DAY_DISCOUNT((anyValue) -> 1_000,
             (date) -> date % 7 == STAR_DAY.getFirstDay() || date == STAR_DAY.getSecondDay()),
     WEEK_DAY_DISCOUNT((dessertMenuCount) -> 2_023 * dessertMenuCount,
-            (date) -> date % 7 != WEEKEND_DAY.getFirstDay() || date % 7 != WEEKEND_DAY.getSecondDay());
+            (date) -> date % 7 != WEEKEND_DAY.getFirstDay() && date % 7 != WEEKEND_DAY.getSecondDay());
 
     private final Function<Integer, Integer> discountAmountCalculator;
     private final Predicate<Integer> dateChecker;
