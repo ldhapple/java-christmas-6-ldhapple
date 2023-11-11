bb4a355c7096dae3d40509a595758dd9eba5aed9
leedohyun <lucky77795@naver.com>
2023-11-12 07:48:54 +0900

    feat(christmas) : 레스토랑에 필요한 계산을 담당하는 RestaurantCalculator 구현.

---
+++ src/main/java/christmas/service/RestaurantCalculator.java
@@ -0,0 +1,25 @@
+package christmas.service;
+
+import christmas.domain.Orders;
+import christmas.domain.food.Food;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class RestaurantCalculator {
+    public int getOrderTotalAmount(Orders orders) {
+        int orderTotalPrice = 0;
+
+        Map<Food, Integer> order = orders.getOrders();
+
+        for (Entry<Food, Integer> entry : order.entrySet())
+        {
+            Food orderFood = entry.getKey();
+            int orderFoodPrice = orderFood.getPrice();
+            int orderFoodCount = entry.getValue();
+
+            orderTotalPrice += orderFoodPrice * orderFoodCount;
+        }
+
+        return orderTotalPrice;
+    }
+}
