62814e87286d488e0d32392f437beb72b06d1bf0
leedohyun <lucky77795@naver.com>
2023-11-15 01:47:27 +0900

    refactor(christmas) : 일정에 따른 할인 정책의 종류를 계산하기 위한 값으로 Integer 대신 VisitDate를 받도록 수정.

--- src/main/java/christmas/domain/event/DiscountPolicy.java
+++ src/main/java/christmas/domain/event/DiscountPolicy.java
@@ -12,35 +12,38 @@ import java.util.stream.Stream;
 public enum DiscountPolicy {
     CHRISTMAS_DISCOUNT("크리스마스 디데이 할인",
             (date) -> 1_000 + ((date - 1) * 100),
-            (date) -> date >= CHRISTMAS_EVENT_PERIOD.getStartDay() && date <= CHRISTMAS_EVENT_PERIOD.getEndDay()),
+            (visitDate) -> visitDate.getDate() >= CHRISTMAS_EVENT_PERIOD.getStartDay()
+                    && visitDate.getDate() <= CHRISTMAS_EVENT_PERIOD.getEndDay()),
     WEEKEND_DISCOUNT("주말 할인",
             (mainMenuCount) -> 2_023 * mainMenuCount,
-            (date) -> date % 7 == WEEKEND_DAY.getStartDay() || date % 7 == WEEKEND_DAY.getEndDay()),
+            (visitDate) -> visitDate.getDate() % 7 == WEEKEND_DAY.getStartDay()
+                    || visitDate.getDate() % 7 == WEEKEND_DAY.getEndDay()),
     STAR_DAY_DISCOUNT("특별 할인",
             (anyValue) -> 1_000,
-            (date) -> date % 7 == STAR_DAY.getStartDay() || date == STAR_DAY.getEndDay()),
+            (visitDate) -> visitDate.getDate() % 7 == STAR_DAY.getStartDay()
+                    || visitDate.getDate() == STAR_DAY.getEndDay()),
     WEEK_DAY_DISCOUNT("평일 할인",
             (dessertMenuCount) -> 2_023 * dessertMenuCount,
-            (date) -> date % 7 != WEEKEND_DAY.getStartDay() && date % 7 != WEEKEND_DAY.getEndDay());
+            (visitDate) -> visitDate.getDate() % 7 != WEEKEND_DAY.getStartDay()
+                    && visitDate.getDate() % 7 != WEEKEND_DAY.getEndDay());
 
     private final String name;
     private final Function<Integer, Integer> discountAmountCalculator;
-    private final Predicate<Integer> dateChecker;
+    private final Predicate<VisitDate> dateChecker;
 
-    DiscountPolicy(String name, Function<Integer, Integer> discountAmountCalculator, Predicate<Integer> dateChecker) {
+    DiscountPolicy(String name, Function<Integer, Integer> discountAmountCalculator, Predicate<VisitDate> dateChecker) {
         this.name = name;
         this.discountAmountCalculator = discountAmountCalculator;
         this.dateChecker = dateChecker;
     }
 
     public static List<DiscountPolicy> findDiscountPolicies(VisitDate visitDate) {
-        int date = visitDate.getDate();
         return Arrays.stream(DiscountPolicy.values())
-                .flatMap(discountPolicy -> policyContainDate(date, discountPolicy))
+                .flatMap(discountPolicy -> policyContainDate(visitDate, discountPolicy))
                 .toList();
     }
 
-    private static Stream<DiscountPolicy> policyContainDate(int date, DiscountPolicy discountPolicy) {
+    private static Stream<DiscountPolicy> policyContainDate(VisitDate date, DiscountPolicy discountPolicy) {
         if (discountPolicy.dateChecker.test(date)) {
             return Stream.of(discountPolicy);
         }
